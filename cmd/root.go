72fe8409c93d4fe947b14d9040a9e36ff8737f0e
Sebastian Schulz <sebastian.schulz95@gmail.com>
2017-02-11 00:03:24 +0100

    input parsing and help output added

--- cmd/root.go
+++ cmd/root.go
@@ -1,34 +1,56 @@
-// Copyright © 2017 Sebastian Schulz <mail@sesc.me>
-
 package cmd
 
 import (
 	"fmt"
+	"image"
+	_ "image/jpeg" // allow use of jpegs
+	_ "image/png"  // allow use of pngs
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var cfgFile string
+type inputImage struct {
+	filepath    string
+	channelMask string // something like "rxxb" would mean that the first channel will be used as red channel, and the alpha channel will be used als blue channel
+	image       image.Image
+}
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "rgba-channel-merge",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "rgba-channel-merge <image-filepath> <image-channel-mask> [(<image-filepath> <image-channel-mask>)...] <output-file-path>",
+	Short: "image channel merge tool",
+	Long: `A tool to merge specific color channels of multiple images into one rgba image.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-// Uncomment the following line if your bare application
-// has an action associated with it:
-//	Run: func(cmd *cobra.Command, args []string) { },
+The channel masks for each image should match the regex [rgbax]{4}, with r, g, b, and a representing the red, green, blue and alpha channel, and x meaning that this channel should be ignored. E.g., the channel mask "rbax" would mean that the first (red) channel of the input image will be used as the red channel for the output image, the second (green) will be used as the blue channel, the third (blue) will be used as alpha channel, and the fourth (alpha) will be ignored.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		inputImages, outputImagePath, err := parseArgs(args)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%#v %#v", inputImages, outputImagePath)
+	},
 }
 
-// Execute adds all child commands to the root command sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+func parseArgs(args []string) (inputImages []inputImage, outputImagePath string, err error) {
+	if len(args) < 3 || len(args)%2 != 1 {
+		err = fmt.Errorf("wrong input format")
+		return
+	}
+
+	for i := 0; i < (len(args)-1)/2; i++ {
+		inputImages = append(inputImages, inputImage{
+			filepath:    args[i*2],
+			channelMask: args[i*2+1],
+		})
+	}
+
+	outputImagePath = args[len(args)-1]
+	return
+}
+
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -37,30 +59,5 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
-	// will be global for your application.
-
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.rgba-channel-merge.yaml)")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" { // enable ability to specify config file via flag
-		viper.SetConfigFile(cfgFile)
-	}
-
-	viper.SetConfigName(".rgba-channel-merge") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")  // adding home directory as first search path
-	viper.AutomaticEnv()          // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// Copyright © 2017 Sebastian Schulz <mail@sesc.me>
-
 package main
 
 import "github.com/blblblu/rgba-channel-merge/cmd"
